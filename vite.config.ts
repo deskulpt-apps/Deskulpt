7b1f68b4f59a6043650f10dc872ac95957abee30
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2025-10-24 00:54:49 -0400

    chore: change identifier to `com.deskulpt.app` (#606)
    * chore: change identifier to com.deskulpt.app
    
    * [ci-build]

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
 import react from "@vitejs/plugin-react";
-import tauriConf from "./crates/deskulpt/tauri.conf.json";
+import { version } from "./package.json";
 
 export default defineConfig({
   define: {
-    __VERSION__: JSON.stringify(tauriConf.version),
+    __VERSION__: JSON.stringify(version),
   },
   plugins: [
     react({
