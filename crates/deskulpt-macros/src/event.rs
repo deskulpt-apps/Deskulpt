deskulpt-apps/Deskulpt

commit 14c4c0742956aaa9dd2d5c3d031a3294006ff7f5

refactor: bindings generation logic (#613)
The most important change is that, instead of generating all bindings
into a single `index.ts` file, each internal plugin is dropped into a
single file and the index is only an entry point. This means:

- `import { Type } from "@deskulpt/bindings"` would become `import {
pluginName } from "@deskulpt/bindings"` then `pluginName.Type` - `import
{ events } from "@deskulpt/bindings"` then `events.eventName` would
become `import { pluginName } from "@deskulpt/bindings"` then
`pluginName.events.eventName` - `import { commands } from
"@deskulpt/bindings"` then `commands.pluginName.commandName` would
become `import { pluginName } from "@deskulpt/bindings"` then
`pluginName.commands.commandName`

Another major change (as is already shown above) is that events are now
also namespaced (as how commands are). The event names are now
`plugin-name://event-name`. This allows duplicate event name in
different internal plugins.

crates/xtask-gen/src/bindings/module.rs
@@ -1,0 +1,177 @@
+use anyhow::Result;
+use deskulpt_common::bindings::Bindings;
+use handlebars::Handlebars;
+use heck::ToLowerCamelCase;
+use regex::Regex;
+use serde::Serialize;
+use specta::datatype::{DataType, Function, FunctionResultVariant};
+use specta::TypeCollection;
+use specta_typescript::{datatype, export_named_datatype, js_doc, Typescript};
+
+mod helpers {
+    use handlebars::{Context, Handlebars, Helper, HelperResult, Output, RenderContext};
+
+    /// Handlebars helper to indent (multi-line) text by a given number of
+    /// spaces.
+    pub fn indent(
+        h: &Helper<'_>,
+        _: &Handlebars,
+        _: &Context,
+        _: &mut RenderContext<'_, '_>,
+        out: &mut dyn Output,
+    ) -> HelperResult {
+        let text = h
+            .param(0)
+            .and_then(|p| p.value().as_str())
+            .unwrap_or_default();
+        let spaces = h
+            .param(1)
+            .and_then(|p| p.value().as_u64())
+            .unwrap_or_default();
+
+        let pad = " ".repeat(spaces as usize);
+        for (i, line) in text.lines().enumerate() {
+            if i > 0 {
+                out.write("\n")?;
+            }
+            out.write(&pad)?;
+            out.write(line)?;
+        }
+
+        Ok(())
+    }
+}
+
+/// Similar to [`export_named_datatype`] but for [`DataType`].
+fn export_datatype(ts: &Typescript, typ: &DataType, tcl: &TypeCollection) -> Result<String> {
+    Ok(datatype(
+        ts,
+        &FunctionResultVariant::Value(typ.clone()),
+        tcl,
+    )?)
+}
+
+#[derive(Debug, Serialize)]
+struct EventTemplate {
+    key: String,
+    name: String,
+    ty: String,
+}
+
+impl EventTemplate {
+    fn from(ts: &Typescript, tcl: &TypeCollection, name: &str, ty: &DataType) -> Result<Self> {
+        Ok(Self {
+            key: name.to_lower_camel_case(),
+            name: name.to_string(),
+            ty: export_datatype(ts, ty, tcl)?,
+        })
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct CommandArgTemplate {
+    name: String,
+    ty: String,
+}
+
+impl CommandArgTemplate {
+    fn from(ts: &Typescript, tcl: &TypeCollection, name: &str, ty: &DataType) -> Result<Self> {
+        Ok(Self {
+            name: name.to_lower_camel_case(),
+            ty: export_datatype(ts, ty, tcl)?,
+        })
+    }
+}
+
+#[derive(Debug, Serialize)]
+struct CommandTemplate {
+    key: String,
+    name: String,
+    args: Vec<CommandArgTemplate>,
+    ret_ty: String,
+    doc: String,
+}
+
+impl CommandTemplate {
+    fn from(ts: &Typescript, tcl: &TypeCollection, function: &Function) -> Result<Self> {
+        Ok(Self {
+            key: function.name().to_lower_camel_case(),
+            name: function.name().to_string(),
+            args: function
+                .args()
+                .map(|(name, ty)| CommandArgTemplate::from(ts, tcl, name, ty))
+                .collect::<Result<Vec<_>>>()?,
+            ret_ty: match function.result() {
+                Some(FunctionResultVariant::Value(t))
+                | Some(FunctionResultVariant::Result(t, _)) => export_datatype(ts, t, tcl)?,
+                None => "void".to_string(),
+            },
+            doc: {
+                let mut builder = js_doc::Builder::default();
+                if let Some(d) = &function.deprecated() {
+                    builder.push_deprecated(d);
+                }
+                if !function.docs().is_empty() {
+                    builder.extend(function.docs().split("\n"));
+                }
+                builder.build()
+            },
+        })
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct Template {
+    pub namespace: &'static str,
+    types: Vec<String>,
+    events: Vec<EventTemplate>,
+    commands: Vec<CommandTemplate>,
+}
+
+impl Template {
+    pub fn from(bindings: Bindings) -> Result<Self> {
+        let ts = Typescript::new();
+
+        Ok(Self {
+            namespace: bindings.namespace,
+            types: bindings
+                .types
+                .into_iter()
+                .map(|(_, ndt)| Ok(export_named_datatype(&ts, ndt, &bindings.types)?))
+                .collect::<Result<Vec<_>>>()?,
+            events: bindings
+                .events
+                .iter()
+                .map(|(name, ty)| EventTemplate::from(&ts, &bindings.types, name, ty))
+                .collect::<Result<Vec<_>>>()?,
+            commands: bindings
+                .commands
+                .iter()
+                .map(|function| CommandTemplate::from(&ts, &bindings.types, function))
+                .collect::<Result<Vec<_>>>()?,
+        })
+    }
+
+    pub fn render(&self) -> Result<()> {
+        let mut hb = Handlebars::new();
+        hb.set_strict_mode(true);
+        hb.register_escape_fn(handlebars::no_escape);
+        hb.register_helper("indent", Box::new(helpers::indent));
+        hb.register_template_string("module", include_str!("module.ts.hbs"))?;
+        let output = hb.render("module", self)?;
+
+        // TODO: Remove when specta > 2.0.0-rc.22
+        let re =
+            Regex::new(r"Partial\s*<\s*(\{\s*\[\s*key\s+in\s+string\s*\][^}]*\})\s*>").unwrap();
+        let output = re.replace_all(&output, "$1").to_string();
+
+        let path = deskulpt_workspace::package_dir("deskulpt-bindings")
+            .join("src")
+            .join(self.namespace)
+            .with_extension("ts");
+        std::fs::write(&path, output)?;
+        println!("✅ Generated: {}", path.display());
+
+        Ok(())
+    }
+}

crates/xtask-gen/src/bindings/module.ts.hbs
@@ -1,7 +1,11 @@
 /*! Auto-generated by xtask-gen. DO NOT EDIT! */
 
+{{#if events}}
 import { invoke } from "@tauri-apps/api/core";
+{{/if}}
+{{#if commands}}
 import * as tauriEvent from "@tauri-apps/api/event";
+{{/if}}
 
 // =============================================================================
 // Types
@@ -14,6 +18,7 @@ import * as tauriEvent from "@tauri-apps/api/event";
 {{/unless}}
 {{/each}}
 
+{{#if events}}
 // =============================================================================
 // Events
 // =============================================================================
@@ -38,32 +43,31 @@ function makeEvent<T>(name: string) {
 
 export const events = {
 {{#each events}}
-  {{this.key}}: makeEvent<{{this.ty}}>("{{this.name}}"),
+  {{this.key}}: makeEvent<{{this.ty}}>("{{../namespace}}://{{this.name}}"),
 {{/each}}
 };
+{{/if}}
 
+{{#if commands}}
 // =============================================================================
 // Commands
 // =============================================================================
 
 export const commands = {
 {{#each commands}}
-  {{@key}}: {
-{{#each this}}
-{{indent doc 4}}
-    {{this.key}}: ({{#if this.args}}
+{{indent this.doc 2}}
+  {{this.key}}: ({{#if this.args}}
 {{#each this.args}}
-      {{this.name}}: {{this.ty}},
+    {{this.name}}: {{this.ty}},
 {{/each}}
-    {{/if}}) => invoke<{{this.ret_ty}}>("plugin:{{this.plugin_name}}|{{this.name}}"{{#if this.args}}, {
+  {{/if}}) => invoke<{{this.ret_ty}}>("plugin:{{../namespace}}|{{this.name}}"{{#if this.args}}, {
 {{#each this.args}}
-      {{this.name}},
+    {{this.name}},
 {{/each}}
-    }{{/if}}),
+  }{{/if}}),
 {{#unless @last}}
 
 {{/unless}}
 {{/each}}
-  },
-{{/each}}
 };
+{{/if}}

packages/deskulpt-bindings/src/deskulpt-core.ts
@@ -1,0 +1,361 @@
+/*! Auto-generated by xtask-gen. DO NOT EDIT! */
+
+import { invoke } from "@tauri-apps/api/core";
+import * as tauriEvent from "@tauri-apps/api/event";
+
+// =============================================================================
+// Types
+// =============================================================================
+
+/**
+ * Deskulpt window enum.
+ */
+export type DeskulptWindow = 
+/**
+ * The manager window.
+ */
+"manager" | 
+/**
+ * The canvas window.
+ */
+"canvas"
+
+export type JsonValue = null | boolean | number | string | JsonValue[] | { [key in string]: JsonValue }
+
+/**
+ * A result-like binary outcome.
+ * 
+ * This represents the outcome of an operation that can either succeed with a
+ * value of type `T` or fail with an error message.
+ */
+export type Outcome<T> = { type: "ok"; content: T } | { type: "err"; content: string }
+
+/**
+ * Event for rendering widgets.
+ * 
+ * This event is emitted from the backend to the canvas window to instruct it
+ * to render the provided widgets. The event carries a mapping from widget IDs
+ * to their corresponding code strings.
+ */
+export type RenderWidgetsEvent = { [key in string]: Outcome<string> }
+
+/**
+ * Full settings of the Deskulpt application.
+ */
+export type Settings = { 
+/**
+ * The application theme.
+ */
+theme: Theme; 
+/**
+ * The keyboard shortcuts.
+ */
+shortcuts: Partial<{ [key in ShortcutKey]: string }>; 
+/**
+ * The mapping from widget IDs to their respective settings.
+ */
+widgets: { [key in string]: WidgetSettings } }
+
+/**
+ * A patch for partial updates to [`Settings`].
+ */
+export type SettingsPatch = { 
+/**
+ * If not `None`, update [`Settings::theme`].
+ */
+theme?: Theme; 
+/**
+ * If not `None`, update [`Settings::shortcuts`].
+ * 
+ * Non-specified shortcuts will remain unchanged. If a shortcut value is
+ * `None`, it means removing that shortcut. Otherwise, it means updating
+ * or adding that shortcut.
+ */
+shortcuts?: Partial<{ [key in ShortcutKey]: string | null }>; 
+/**
+ * If not `None`, update [`Settings::widgets`].
+ * 
+ * Non-specified widgets will remain unchanged. If a widget settings patch
+ * is `None`, it means leaving that widget settings unchanged. Otherwise,
+ * it means applying the patch to that widget settings. If the widget ID
+ * does not exist, a new widget settings will be created with default
+ * values, and then the patch will be applied to it.
+ */
+widgets?: { [key in string]: WidgetSettingsPatch | null } }
+
+/**
+ * Types of keyboard shortcuts in the application.
+ */
+export type ShortcutKey = 
+/**
+ * For toggling canvas interaction mode.
+ */
+"toggleCanvasImode" | 
+/**
+ * For opening the manager window.
+ */
+"openManager"
+
+/**
+ * Event for showing a toast notification.
+ * 
+ * This event is emitted from the backend to the canvas window when a toast
+ * notification needs to be displayed.
+ */
+export type ShowToastEvent = 
+/**
+ * Show a [success](https://sonner.emilkowal.ski/toast#success) toast.
+ */
+{ type: "success"; content: string } | 
+/**
+ * Show an [error](https://sonner.emilkowal.ski/toast#error) toast.
+ */
+{ type: "error"; content: string }
+
+/**
+ * Light/dark theme of the application.
+ */
+export type Theme = "light" | "dark"
+
+/**
+ * Event for updating the settings.
+ * 
+ * This event is emitted from the backend to all frontend windows whenever
+ * there is a change in the settings. Full settings are included to ensure
+ * that all windows see the most up-to-date version eventually.
+ */
+export type UpdateSettingsEvent = Settings
+
+/**
+ * Event for updating the widget catalog.
+ * 
+ * This event is emitted from the backend to all frontend windows whenever
+ * there is a change in the widget catalog.
+ */
+export type UpdateWidgetCatalogEvent = WidgetCatalog
+
+/**
+ * The widget catalog.
+ * 
+ * This is a collection of all widgets discovered locally, mapped from their
+ * widget IDs to their configurations. Invalid widgets are also included with
+ * their error messages.
+ */
+export type WidgetCatalog = { [key in string]: Outcome<WidgetConfig> }
+
+/**
+ * Full configuration of a Deskulpt widget.
+ */
+export type WidgetConfig = { 
+/**
+ * The name of the widget.
+ */
+name: string; 
+/**
+ * The entry point of the widget.
+ */
+entry: string; 
+/**
+ * The dependencies of the widget.
+ */
+dependencies: { [key in string]: string } }
+
+/**
+ * Per-widget settings.
+ * 
+ * Different from widget configurations, these are independent of the widget
+ * configuration files and are managed internally by the application.
+ */
+export type WidgetSettings = { 
+/**
+ * The leftmost x-coordinate in pixels.
+ */
+x: number; 
+/**
+ * The topmost y-coordinate in pixels.
+ */
+y: number; 
+/**
+ * The width in pixels.
+ */
+width: number; 
+/**
+ * The height in pixels.
+ */
+height: number; 
+/**
+ * The opacity in percentage.
+ */
+opacity: number }
+
+/**
+ * A patch for partial updates to [`WidgetSettings`].
+ */
+export type WidgetSettingsPatch = { 
+/**
+ * If not `None`, update [`WidgetSettings::x`].
+ */
+x?: number; 
+/**
+ * If not `None`, update [`WidgetSettings::y`].
+ */
+y?: number; 
+/**
+ * If not `None`, update [`WidgetSettings::width`].
+ */
+width?: number; 
+/**
+ * If not `None`, update [`WidgetSettings::height`].
+ */
+height?: number; 
+/**
+ * If not `None`, update [`WidgetSettings::opacity`].
+ */
+opacity?: number }
+
+// =============================================================================
+// Events
+// =============================================================================
+
+function makeEvent<T>(name: string) {
+  return {
+    /** The name of the event. */
+    name,
+    /** Listen for the event. */
+    listen: (cb: tauriEvent.EventCallback<T>, options?: tauriEvent.Options) =>
+      tauriEvent.listen(name, cb, options),
+    /** Listen once for the event. */
+    once: (cb: tauriEvent.EventCallback<T>, options?: tauriEvent.Options) =>
+      tauriEvent.once(name, cb, options),
+    /** Emit the event to all targets. */
+    emit: (payload: T) => tauriEvent.emit(name, payload),
+    /** Emit the event to a specific Deskulpt window. */
+    emitTo: (window: DeskulptWindow, payload: T) =>
+      tauriEvent.emitTo(window, name, payload),
+  };
+}
+
+export const events = {
+  renderWidgets: makeEvent<RenderWidgetsEvent>("deskulpt-core://render-widgets"),
+  showToast: makeEvent<ShowToastEvent>("deskulpt-core://show-toast"),
+  updateSettings: makeEvent<UpdateSettingsEvent>("deskulpt-core://update-settings"),
+  updateWidgetCatalog: makeEvent<UpdateWidgetCatalogEvent>("deskulpt-core://update-widget-catalog"),
+};
+
+// =============================================================================
+// Commands
+// =============================================================================
+
+export const commands = {
+  /**
+   * Bundle widgets.
+   * 
+   * This command bundles the specified widgets that exist in the catalog. If
+   * `ids` is not provided, all widgets in the catalog are bundled. Failure to
+   * bundle an individual widget does not prevent other widgets from being
+   * bundled. Instead, the outcome of each bundling operation is collected and
+   * sent to the canvas window via the [`RenderWidgetsEvent`].
+   * 
+   * ### Errors
+   * 
+   * - Error accessing the widgets directory.
+   * - Error emitting the [`RenderWidgetsEvent`].
+   */
+  bundleWidgets: (
+    ids: string[] | null,
+  ) => invoke<null>("plugin:deskulpt-core|bundle_widgets", {
+    ids,
+  }),
+
+  /**
+   * Mark the window to have completed its setup.
+   * 
+   * If all setup has been completed after marking this window as completed, this
+   * command will automatically trigger an initial rescan of the widgets.
+   * 
+   * ### Errors
+   * 
+   * - Error rescanning the widgets (if applicable).
+   */
+  completeSetup: () => invoke<null>("plugin:deskulpt-core|complete_setup"),
+
+  /**
+   * Call a plugin command (🚧 TODO 🚧).
+   * 
+   * ### 🚧 TODO 🚧
+   * 
+   * The Deskulpt core should keep a state of the registered plugins and call the
+   * plugins dynamically. Also, instead of invoking the plugins directly, the
+   * Deskulpt core should not depend on any of the plugins and should use IPC to
+   * communicate with the plugins.
+   * 
+   * Also, in order to simplify the engine API for the plugin (because it is
+   * a temporary implementation), `app_handle` is using the default runtime but
+   * it should be a generic `R: Runtime` parameter in the final implementation.
+   */
+  callPlugin: (
+    plugin: string,
+    command: string,
+    id: string,
+    payload: JsonValue | null,
+  ) => invoke<JsonValue>("plugin:deskulpt-core|call_plugin", {
+    plugin,
+    command,
+    id,
+    payload,
+  }),
+
+  /**
+   * Open the widgets directory or a specific widget directory.
+   * 
+   * If the widget ID is provided, a specific widget directory will be opened.
+   * Otherwise, the widgets directory will be opened.
+   * 
+   * ### Errors
+   * 
+   * - Widget ID is provided but does not exist in the collection.
+   * - Failed to access the widgets directory.
+   * - Error opening the directory.
+   */
+  openWidget: (
+    id: string | null,
+  ) => invoke<null>("plugin:deskulpt-core|open_widget", {
+    id,
+  }),
+
+  /**
+   * Rescan the widgets directory to discover widgets.
+   * 
+   * This command scans the widgets directory for available widgets and updates
+   * the widget catalog and settings accordingly. It then emits events to notify
+   * the frontend of these changes. Finally, it triggers the bundling of all
+   * widgets in the updated catalog with `bundle_widgets` to ensure they are
+   * ready for use.
+   * 
+   * ### Errors
+   * 
+   * - Error accessing the widgets directory.
+   * - Error loading the new widget catalog from the widgets directory.
+   * - Error emitting the [`UpdateSettingsEvent`].
+   * - Error emitting the [`UpdateWidgetCatalogEvent`].
+   * - Error bundling all discovered widgets.
+   */
+  rescanWidgets: () => invoke<null>("plugin:deskulpt-core|rescan_widgets"),
+
+  /**
+   * Update the settings.
+   * 
+   * This command updates the settings state in the backend. If an update has
+   * side effects, they will be applied prior to the update being committed. See
+   * [`SettingsStateExt`] for more information.
+   * 
+   * ### Errors
+   * 
+   * - Failed to apply the side effects, if any.
+   */
+  updateSettings: (
+    patch: SettingsPatch,
+  ) => invoke<null>("plugin:deskulpt-core|update_settings", {
+    patch,
+  }),
+};

.prettierignore
@@ -5,5 +5,5 @@ Cargo.lock
 # Auto-generated files
 crates/deskulpt-core/gen/
 gen/
-packages/deskulpt-bindings/src/index.ts
+packages/deskulpt-bindings/src/
 docs/src/public/settings-schema.json

commit a94a9a3cdb1a35bb2236eb33fbc87e84b86339ff

fix: broken event system introduced in 14c4c07

crates/deskulpt-macros/src/event.rs
@@ -37,7 +37,7 @@ pub fn proc_derive_event(input: TokenStream) -> TokenStream {
 
     let expanded = quote! {
         impl #impl_generics ::deskulpt_common::event::Event for #ident #ty_generics #where_clause {
-            const NAME: &str = #lit;
+            const NAME: &str = concat!(env!("CARGO_PKG_NAME"), "://", #lit);
         }
     };
     TokenStream::from(expanded)

crates/xtask-gen/src/bindings/module.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use deskulpt_common::bindings::Bindings;
 use handlebars::Handlebars;
 use heck::ToLowerCamelCase;
@@ -61,7 +61,13 @@ struct EventTemplate {
 impl EventTemplate {
     fn from(ts: &Typescript, tcl: &TypeCollection, name: &str, ty: &DataType) -> Result<Self> {
         Ok(Self {
-            key: name.to_lower_camel_case(),
+            key: name
+                .split("://")
+                .nth(1)
+                .ok_or_else(|| {
+                    anyhow!("Deskulpt event name must contain '://' separator, but got: {name}")
+                })?
+                .to_lower_camel_case(),
             name: name.to_string(),
             ty: export_datatype(ts, ty, tcl)?,
         })

crates/xtask-gen/src/bindings/module.ts.hbs
@@ -43,7 +43,7 @@ function makeEvent<T>(name: string) {
 
 export const events = {
 {{#each events}}
-  {{this.key}}: makeEvent<{{this.ty}}>("{{../namespace}}://{{this.name}}"),
+  {{this.key}}: makeEvent<{{this.ty}}>("{{this.name}}"),
 {{/each}}
 };
 {{/if}}

packages/deskulpt-bindings/src/deskulpt-core.ts
@@ -10,11 +10,11 @@ import * as tauriEvent from "@tauri-apps/api/event";
 /**
  * Deskulpt window enum.
  */
-export type DeskulptWindow = 
+export type DeskulptWindow =
 /**
  * The manager window.
  */
-"manager" | 
+"manager" |
 /**
  * The canvas window.
  */
@@ -24,7 +24,7 @@ export type JsonValue = null | boolean | number | string | JsonValue[] | { [key 
 
 /**
  * A result-like binary outcome.
- * 
+ *
  * This represents the outcome of an operation that can either succeed with a
  * value of type `T` or fail with an error message.
  */
@@ -32,7 +32,7 @@ export type Outcome<T> = { type: "ok"; content: T } | { type: "err"; content: st
 
 /**
  * Event for rendering widgets.
- * 
+ *
  * This event is emitted from the backend to the canvas window to instruct it
  * to render the provided widgets. The event carries a mapping from widget IDs
  * to their corresponding code strings.
@@ -42,15 +42,15 @@ export type RenderWidgetsEvent = { [key in string]: Outcome<string> }
 /**
  * Full settings of the Deskulpt application.
  */
-export type Settings = { 
+export type Settings = {
 /**
  * The application theme.
  */
-theme: Theme; 
+theme: Theme;
 /**
  * The keyboard shortcuts.
  */
-shortcuts: Partial<{ [key in ShortcutKey]: string }>; 
+shortcuts: Partial<{ [key in ShortcutKey]: string }>;
 /**
  * The mapping from widget IDs to their respective settings.
  */
@@ -59,22 +59,22 @@ widgets: { [key in string]: WidgetSettings } }
 /**
  * A patch for partial updates to [`Settings`].
  */
-export type SettingsPatch = { 
+export type SettingsPatch = {
 /**
  * If not `None`, update [`Settings::theme`].
  */
-theme?: Theme; 
+theme?: Theme;
 /**
  * If not `None`, update [`Settings::shortcuts`].
- * 
+ *
  * Non-specified shortcuts will remain unchanged. If a shortcut value is
  * `None`, it means removing that shortcut. Otherwise, it means updating
  * or adding that shortcut.
  */
-shortcuts?: Partial<{ [key in ShortcutKey]: string | null }>; 
+shortcuts?: Partial<{ [key in ShortcutKey]: string | null }>;
 /**
  * If not `None`, update [`Settings::widgets`].
- * 
+ *
  * Non-specified widgets will remain unchanged. If a widget settings patch
  * is `None`, it means leaving that widget settings unchanged. Otherwise,
  * it means applying the patch to that widget settings. If the widget ID
@@ -86,11 +86,11 @@ widgets?: { [key in string]: WidgetSettingsPatch | null } }
 /**
  * Types of keyboard shortcuts in the application.
  */
-export type ShortcutKey = 
+export type ShortcutKey =
 /**
  * For toggling canvas interaction mode.
  */
-"toggleCanvasImode" | 
+"toggleCanvasImode" |
 /**
  * For opening the manager window.
  */
@@ -98,15 +98,15 @@ export type ShortcutKey =
 
 /**
  * Event for showing a toast notification.
- * 
+ *
  * This event is emitted from the backend to the canvas window when a toast
  * notification needs to be displayed.
  */
-export type ShowToastEvent = 
+export type ShowToastEvent =
 /**
  * Show a [success](https://sonner.emilkowal.ski/toast#success) toast.
  */
-{ type: "success"; content: string } | 
+{ type: "success"; content: string } |
 /**
  * Show an [error](https://sonner.emilkowal.ski/toast#error) toast.
  */
@@ -119,7 +119,7 @@ export type Theme = "light" | "dark"
 
 /**
  * Event for updating the settings.
- * 
+ *
  * This event is emitted from the backend to all frontend windows whenever
  * there is a change in the settings. Full settings are included to ensure
  * that all windows see the most up-to-date version eventually.
@@ -128,7 +128,7 @@ export type UpdateSettingsEvent = Settings
 
 /**
  * Event for updating the widget catalog.
- * 
+ *
  * This event is emitted from the backend to all frontend windows whenever
  * there is a change in the widget catalog.
  */
@@ -136,7 +136,7 @@ export type UpdateWidgetCatalogEvent = WidgetCatalog
 
 /**
  * The widget catalog.
- * 
+ *
  * This is a collection of all widgets discovered locally, mapped from their
  * widget IDs to their configurations. Invalid widgets are also included with
  * their error messages.
@@ -146,15 +146,15 @@ export type WidgetCatalog = { [key in string]: Outcome<WidgetConfig> }
 /**
  * Full configuration of a Deskulpt widget.
  */
-export type WidgetConfig = { 
+export type WidgetConfig = {
 /**
  * The name of the widget.
  */
-name: string; 
+name: string;
 /**
  * The entry point of the widget.
  */
-entry: string; 
+entry: string;
 /**
  * The dependencies of the widget.
  */
@@ -162,27 +162,27 @@ dependencies: { [key in string]: string } }
 
 /**
  * Per-widget settings.
- * 
+ *
  * Different from widget configurations, these are independent of the widget
  * configuration files and are managed internally by the application.
  */
-export type WidgetSettings = { 
+export type WidgetSettings = {
 /**
  * The leftmost x-coordinate in pixels.
  */
-x: number; 
+x: number;
 /**
  * The topmost y-coordinate in pixels.
  */
-y: number; 
+y: number;
 /**
  * The width in pixels.
  */
-width: number; 
+width: number;
 /**
  * The height in pixels.
  */
-height: number; 
+height: number;
 /**
  * The opacity in percentage.
  */
@@ -191,23 +191,23 @@ opacity: number }
 /**
  * A patch for partial updates to [`WidgetSettings`].
  */
-export type WidgetSettingsPatch = { 
+export type WidgetSettingsPatch = {
 /**
  * If not `None`, update [`WidgetSettings::x`].
  */
-x?: number; 
+x?: number;
 /**
  * If not `None`, update [`WidgetSettings::y`].
  */
-y?: number; 
+y?: number;
 /**
  * If not `None`, update [`WidgetSettings::width`].
  */
-width?: number; 
+width?: number;
 /**
  * If not `None`, update [`WidgetSettings::height`].
  */
-height?: number; 
+height?: number;
 /**
  * If not `None`, update [`WidgetSettings::opacity`].
  */
@@ -249,15 +249,15 @@ export const events = {
 export const commands = {
   /**
    * Bundle widgets.
-   * 
+   *
    * This command bundles the specified widgets that exist in the catalog. If
    * `ids` is not provided, all widgets in the catalog are bundled. Failure to
    * bundle an individual widget does not prevent other widgets from being
    * bundled. Instead, the outcome of each bundling operation is collected and
    * sent to the canvas window via the [`RenderWidgetsEvent`].
-   * 
+   *
    * ### Errors
-   * 
+   *
    * - Error accessing the widgets directory.
    * - Error emitting the [`RenderWidgetsEvent`].
    */
@@ -269,26 +269,26 @@ export const commands = {
 
   /**
    * Mark the window to have completed its setup.
-   * 
+   *
    * If all setup has been completed after marking this window as completed, this
    * command will automatically trigger an initial rescan of the widgets.
-   * 
+   *
    * ### Errors
-   * 
+   *
    * - Error rescanning the widgets (if applicable).
    */
   completeSetup: () => invoke<null>("plugin:deskulpt-core|complete_setup"),
 
   /**
    * Call a plugin command (🚧 TODO 🚧).
-   * 
+   *
    * ### 🚧 TODO 🚧
-   * 
+   *
    * The Deskulpt core should keep a state of the registered plugins and call the
    * plugins dynamically. Also, instead of invoking the plugins directly, the
    * Deskulpt core should not depend on any of the plugins and should use IPC to
    * communicate with the plugins.
-   * 
+   *
    * Also, in order to simplify the engine API for the plugin (because it is
    * a temporary implementation), `app_handle` is using the default runtime but
    * it should be a generic `R: Runtime` parameter in the final implementation.
@@ -307,12 +307,12 @@ export const commands = {
 
   /**
    * Open the widgets directory or a specific widget directory.
-   * 
+   *
    * If the widget ID is provided, a specific widget directory will be opened.
    * Otherwise, the widgets directory will be opened.
-   * 
+   *
    * ### Errors
-   * 
+   *
    * - Widget ID is provided but does not exist in the collection.
    * - Failed to access the widgets directory.
    * - Error opening the directory.
@@ -325,15 +325,15 @@ export const commands = {
 
   /**
    * Rescan the widgets directory to discover widgets.
-   * 
+   *
    * This command scans the widgets directory for available widgets and updates
    * the widget catalog and settings accordingly. It then emits events to notify
    * the frontend of these changes. Finally, it triggers the bundling of all
    * widgets in the updated catalog with `bundle_widgets` to ensure they are
    * ready for use.
-   * 
+   *
    * ### Errors
-   * 
+   *
    * - Error accessing the widgets directory.
    * - Error loading the new widget catalog from the widgets directory.
    * - Error emitting the [`UpdateSettingsEvent`].
@@ -344,13 +344,13 @@ export const commands = {
 
   /**
    * Update the settings.
-   * 
+   *
    * This command updates the settings state in the backend. If an update has
    * side effects, they will be applied prior to the update being committed. See
    * [`SettingsStateExt`] for more information.
-   * 
+   *
    * ### Errors
-   * 
+   *
    * - Failed to apply the side effects, if any.
    */
   updateSettings: (