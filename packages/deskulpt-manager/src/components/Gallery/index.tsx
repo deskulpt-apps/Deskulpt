8e56dd04f3f7256f8908b00b87d9486763786980
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2025-12-08 19:25:09 -0500

    refactor: implement gallery for widgets (early support) (#716)
    * refactor: implement deskulpt widgets registry (early support)
    
    * make it work!
    
    * add homepage to registry and revamp gallery
    
    * preview ui
    
    * missing import
    
    * clippy
    
    * fix some stupid auto import...
    
    * remove unrelevant changes
    
    * some ui/icon improvements
    
    * more reasonable ui
    
    * add version picker; refactor logic into stores and hooks
    
    Signed-off-by: Charlie-XIAO <yx2436@nyu.edu>
    
    * add version picker; refactor logic into stores and hooks
    
    Signed-off-by: Charlie-XIAO <yx2436@nyu.edu>
    
    * some react fixes
    
    * god knows what i'm doing
    
    * fix oxlint
    
    ---------
    
    Signed-off-by: Charlie-XIAO <yx2436@nyu.edu>

---
+++ packages/deskulpt-manager/src/components/Gallery/index.tsx
@@ -0,0 +1,80 @@
+import { Box, Flex, ScrollArea, Spinner, Text } from "@radix-ui/themes";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { memo, useEffect, useRef } from "react";
+import Header from "./Header";
+import WidgetCard from "./WidgetCard";
+import WidgetPreview from "./WidgetPreview";
+import WidgetVersionPicker from "./WidgetVersionPicker";
+import { useWidgetsGalleryStore } from "../../hooks";
+
+const Gallery = memo(() => {
+  const parentRef = useRef<HTMLDivElement>(null);
+
+  const numWidgets = useWidgetsGalleryStore((state) => state.widgets.length);
+  const isFetching = useWidgetsGalleryStore((state) => state.isFetching);
+  const refresh = useWidgetsGalleryStore((state) => state.refresh);
+
+  useEffect(() => {
+    refresh();
+  }, [refresh]);
+
+  const rowVirtualizer = useVirtualizer({
+    count: numWidgets,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 100,
+    overscan: 5,
+  });
+
+  return (
+    <Flex height="100%" direction="column" px="1" gap="3">
+      <Header refresh={refresh} />
+
+      {isFetching ? (
+        <Flex
+          height="100%"
+          width="100%"
+          align="center"
+          justify="center"
+          gap="3"
+          pb="9"
+        >
+          <Spinner size="2" />
+          <Text size="2">Loading...</Text>
+        </Flex>
+      ) : (
+        <Flex flexGrow="1" minHeight="0">
+          <ScrollArea ref={parentRef} scrollbars="vertical" type="scroll">
+            <Box
+              width="100%"
+              position="relative"
+              style={{ height: rowVirtualizer.getTotalSize() }}
+            >
+              {rowVirtualizer.getVirtualItems().map((row) => {
+                return (
+                  <Box
+                    key={row.key}
+                    position="absolute"
+                    top="0"
+                    left="0"
+                    right="0"
+                    style={{
+                      height: `${row.size}px`,
+                      transform: `translateY(${row.start}px)`,
+                    }}
+                  >
+                    <WidgetCard index={row.index} />
+                  </Box>
+                );
+              })}
+            </Box>
+          </ScrollArea>
+        </Flex>
+      )}
+
+      <WidgetPreview />
+      <WidgetVersionPicker />
+    </Flex>
+  );
+});
+
+export default Gallery;
