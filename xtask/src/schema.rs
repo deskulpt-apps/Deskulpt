72e7acdf2de1971b33c4da81578bfaf8301c76e1
Charlie-XIAO <yx2436@nyu.edu>
2026-01-27 21:55:53 -0500

    refactor: ship settings schema in resources and refer with file://
    Signed-off-by: Charlie-XIAO <yx2436@nyu.edu>

--- xtask/src/schema.rs
+++ xtask/src/schema.rs
@@ -1,13 +1,18 @@
+use std::fs::File;
+
 use anyhow::Result;
 use schemars::schema_for;
 
 pub fn run() -> Result<()> {
-    let schema = schema_for!(deskulpt_settings::Settings);
-    let output = serde_json::to_string_pretty(&schema)?;
+    let schemas = vec![("settings", schema_for!(deskulpt_settings::Settings))];
 
-    let path = deskulpt_workspace::root_dir().join("public/gen/settings-schema.json");
-    std::fs::write(&path, output)?;
-    println!("✅ Generated: {}", path.display());
+    let schema_dir = deskulpt_workspace::root_dir().join("resources/schema");
+    for schema in schemas {
+        let path = schema_dir.join(schema.0).with_extension("json");
+        let file = File::create(&path)?;
+        serde_json::to_writer_pretty(file, &schema.1)?;
+        println!("✅ Generated: {}", path.display());
+    }
 
     Ok(())
 }
