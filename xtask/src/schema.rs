3fc2d2cb6bb327b2526c9639b1b7d35105aefcc8
Charlie-XIAO <yx2436@nyu.edu>
2026-02-03 23:12:45 -0500

    chore: minor changes to xtask schema generation
    Signed-off-by: Charlie-XIAO <yx2436@nyu.edu>

--- xtask/src/schema.rs
+++ xtask/src/schema.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::io::BufWriter;
+
 use anyhow::Result;
 use schemars::schema_for;
 
@@ -7,8 +10,9 @@ pub fn run() -> Result<()> {
     let schema_dir = deskulpt_workspace::root_dir().join("resources/schema");
     for schema in schemas {
         let path = schema_dir.join(schema.0).with_extension("json");
-        let content = serde_json::to_string(&schema.1)?;
-        std::fs::write(&path, content + "\n")?;
+        let file = File::create(&path)?;
+        let writer = BufWriter::new(file);
+        serde_json::to_writer(writer, &schema.1)?;
         println!("✅ Generated: {}", path.display());
     }
 
